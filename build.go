e5cd225aee37d8d13e8b97de57e6b2eb00965907
Samige105 <samige10567@gmail.com>
2023-09-19 09:47:41 +1200

    added some tasks to do

---
+++ build.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func Build() {
+	// TODO: Build database
+	fmt.Println("For now, i just print this :)")
+}
--- main.go
+++ main.go
@@ -6,5 +6,10 @@ import (
 
 func main() {
 	fmt.Println("Hello world!")
-	fmt.Println("This line was made on Aidan's computer, not James'")
+	database := false
+	// Change this to make it so the database will be built if it doesn't exist. maybe call an error if database hasn't been started or if it exists?
+	if (database == false) { 
+		fmt.Println("Building database...")
+		Build()
+	}
 }
